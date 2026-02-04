985c76d9c3deef95e68b216acc38f66ecda5f855
Martin Jönsson <martin.t.joensson@gmail.com>
2026-02-04 16:28:48 +0100

    Add MongoDB and User Model

---
+++ src/models/User.ts
@@ -0,0 +1,25 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IUser extends Document {
+  email: string;
+  password: string;
+  createdAt: Date;
+}
+
+const UserSchema = new Schema<IUser>(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const User = mongoose.model<IUser>("User", UserSchema);
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,19 @@
+import "dotenv/config"; // 👈 MUST be first
+
 import app from "./app";
+import { connectDB } from "./config/db";
+
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const startServer = async () => {
+  await connectDB();
+
+
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
+
+startServer();
